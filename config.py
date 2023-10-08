2947432946330cf44c764574246589fd33050d82
1FR13NDs <100146832+1FR13NDs@users.noreply.github.com>
2023-10-09 01:18:29 +0300

    config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # Bot Token
-TOKEN = "MTE1MjY1NzU3MTk1MDM2MjY1NA.GHPWyS.PKLx0NDtZc5rhnDplV6CcEy2psx-KqIWe-faOE"
+TOKEN = ""
 
 # Belirlenen Kanal ID
 CHANNEL_ID = 1142075585233965118  # Kanalın gerçek ID'sini buraya yazın
 
 # Belirli Kullanıcılar için DM Erişimi
-AUTHORIZED_USERS = [537037278933942282,409715111213662209]  # Yetkilendirilmiş kullanıcıların ID'lerini buraya ekleyin
+AUTHORIZED_USERS = []  # Yetkilendirilmiş kullanıcıların ID'lerini buraya ekleyin
 
 # Maksimum Hesap Sayısı
 MAX_ACCOUNTS_TO_SEND = 10
@@ -14,7 +14,7 @@ MAX_ACCOUNTS_TO_SEND = 10
 ACCOUNTS_FILE = "hesaplar.txt"
 
 # VIP Kullanıcılar
-VIP_USERS = [537037278933942282,409715111213662209]  # VIP kullanıcıların ID'lerini buraya ekleyin
+VIP_USERS = []  # VIP kullanıcıların ID'lerini buraya ekleyin
 
 # VIP Hesaplar Dosyası
 VIP_ACCOUNTS_FILE = "2018.txt"
